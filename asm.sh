8bb24d633891f3838775d214d28cb2101aef7f2e
Bradley Gannon <bradley@bradleygannon.com>
2026-01-31 10:24:49 -0500

    Fix width parameter in asm.sh

--- asm.sh
+++ asm.sh
@@ -7,7 +7,7 @@ TMPBIN="/tmp/tmp.bin"
 
 llvm-mc -triple=riscv32 -filetype=obj -o "$TMPOBJ" "$1"
 llvm-objcopy -O binary -j .text "$TMPOBJ" "$TMPBIN"
-od -An -tx4 -w -v "$TMPBIN" | sed "s/^ *//"
+od -An -tx4 -w4 -v "$TMPBIN" | sed "s/^ *//"
 
 rm "$TMPOBJ" "$TMPBIN"
 
